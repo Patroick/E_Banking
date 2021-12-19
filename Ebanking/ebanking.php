a43c86dd5bb2ab79b3e7c671d0df0a6dc7286a16
Patroick <43811779+Patroick@users.noreply.github.com>
2021-12-19 20:17:09 +0100

    Vorbereitung auf Datenbankzugriff 19.12.2021
    Co-authored-by: Florian <FlorianJester@users.noreply.github.com>

--- Ebanking/ebanking.php
+++ Ebanking/ebanking.php
@@ -4,17 +4,29 @@
 <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossorigin="anonymous">
 
     <title>E-Banking App</title>
 </head>
 
 <body>
 
+    <?php 
+    if(isset($_POST['logout'])){
+        session_destroy();
+        header("Location: ../index.php");
+    }
+    ?>
+
     <div class="container">
-        <div class="row" style="text-align: center">
+        <div class="row"
+             style="text-align: center">
             <div class="col-md-12 mt-3">
                 <h1>E-Banking App</h1>
             </div>
@@ -22,39 +34,55 @@
 
         <div class="row">
 
-            <div class="col-sm-3 border rounded m-1 pt-3" style="text-align: center; max-height: 25em">
+            <div class="col-sm-3 border rounded m-1 pt-3"
+                 style="text-align: center; max-height: 22em">
                 <h3> Ihr Profil </h3>
-                <img src="../Img/unknown.png" alt="Profilbild" style="width: 6em; height: 6em">
+                <img src="../Img/unknown.png"
+                     alt="Profilbild"
+                     style="width: 6em; height: 6em">
                 <p class="pt-3">Huber Robert</p>
                 <p>hu.rob@tsn.at</p>
-                <button type="button" class="btn btn-primary mb-3">Ausloggen</button>
+                <form name="logout"
+                      action="ebanking.php"
+                      method="post">
+                    <button type="submit"
+                            name="logout"
+                            class="btn btn-primary mb-3">Ausloggen</button>
+                </form>
             </div>
 
-            <div class=" col-sm-3 border rounded m-1 text-truncate pt-3" style="text-align: center; max-height: 25em">
+            <div class=" col-sm-3 border rounded m-1 text-truncate pt-3"
+                 style="text-align: center; max-height: 22em">
                 <h3>Ihr Konto</h3>
                 <p>AT-411100000237571500</p>
-                <?php
-                if (isset($_POST['transaction'])) {
-                    print_r("here");
-                }
-                ?>
+
                 <p>Spar-Konto</p>
                 <p>Kontostand: 0,00 €</p>
-                <form name="transaction" id="transaction" action="ebanking.php" method="post">
-                    <button type="submit" class="btn btn-primary">Überweisung</button></br></br>
+                <form name="transaction"
+                      id="transaction"
+                      action="ebanking.php"
+                      method="post">
+                    <button type="submit"
+                            name="transaction"
+                            class="btn btn-primary">Überweisung</button></br></br>
                 </form>
-                <form name="addBalance" action="" method="post">
-                    <button type="submit" class="btn btn-secondary" style="background-color: green">Geld aufladen</button></br></br>
-                </form>
-                <form name="removeBalance" action="" method="post">
-                    <button type="submit" class="btn btn-secondary mb-3" style="background-color: orange">Geld abheben</button>
+                <form name="transactionHistory"
+                      action="ebanking.php"
+                      method="post">
+                    <button type="submit"
+                            name="transactionHistory"
+                            class="btn btn-secondary"
+                            style="background-color: green">Überweisungshistorie</button></br></br>
                 </form>
             </div>
-            <div class="col-sm-5 border rounded m-1 p-3" style="max-height: 25em">
-                <h3>Transaktionen</h3>
-                <div class="table-responsive" style="max-height: 20em">
+            <div class="col-sm-5 border rounded m-1 p-3"
+                 style="max-height: 22em">
+                <h3>Letzte Transaktionen</h3>
+                <div class="table-responsive"
+                     style="max-height: 17em">
                     <table class="table table-hover">
-                        <thead class="thead-dark">
+                        <thead class="thead-dark"
+                               style="position: sticky;">
                             <tr>
                                 <th scope="col">Datum</td>
                                 <th scope="col">Empfänger</td>
@@ -87,31 +115,166 @@
                                 <td>AT-12348395324</td>
                                 <td>700.00€</td>
                             </tr>
-                            <tr>
-                                <td>01.02.2022</td>
-                                <td>AT-12348395324</td>
-                                <td>700.00€</td>
-                            </tr>
-                            <tr>
-                                <td>01.02.2022</td>
-                                <td>AT-12348395324</td>
-                                <td>700.00€</td>
-                            </tr>
-                            <tr>
-                                <td>01.02.2022</td>
-                                <td>AT-12348395324</td>
-                                <td>700.00€</td>
-                            </tr>
-                            <tr>
-                                <td>01.02.2022</td>
-                                <td>AT-12348395324</td>
-                                <td>700.00€</td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-    </div>
+
+        <div class="row">
+            <?php
+                if(isset($_POST['transaction'])){ ?>
+            <form name="transactionForm"
+                  action="ebanking.php"
+                  method="post">
+                <div class="col-sm-11 border rounded m-1 pt-3 container">
+                    <div class="form-group">
+                        <label for="recipient">IBAN</label>
+                        <input type="text"
+                               class="form-control"
+                               id="recipient"
+                               name="recipient"
+                               placeholder="AT-XXXXXXXXXXXXXXXXXX">
+                    </div>
+                    <div class="form-group pt-2 pb-1">
+                        <label for="amount">Betrag in €</label>
+                        <input type="text"
+                               class="form-control"
+                               id="amount"
+                               name="amount"
+                               placeholder="0.00">
+                    </div>
+                    <button type="submit"
+                            class="btn btn-primary mb-2 mt-2">Überweisung</button>
+                </div>
+                <?php
+                }
+                if(isset($_POST['transactionHistory'])){ ?>
+                <div class="col-sm-11 border rounded m-1 p-3"
+                     style="max-height: 30em">
+                    <h3>Überweisungshistorie</h3>
+                    <div class="row ">
+                        <div class="col-sm-3">
+
+                            <form name="sortByDate"
+                                  action="ebanking.php"
+                                  method="post">
+                                <label for="sortByDate">Von</label>
+                                <input type="date"
+                                       name="sortByDate"
+                                       class="form-control">
+                                <label for="sortByDate">Bis</label>
+                                <input type="date"
+                                       name="sortByDate"
+                                       class="form-control">
+
+
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="row">
+                                <form name="sortByText"
+                                      action="ebanking.php"
+                                      method="post">
+                                    <label for="sortByText">Text</label>
+                                    <input type="text"
+                                           name="sortByText"
+                                           class="form-control">
+
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <form name="sortByDate"
+                                  action="ebanking.php"
+                                  method="post">
+                                <label for="sortByAmount">Min</label>
+                                <input type="text"
+                                       name="sortByAmount"
+                                       class="form-control"
+                                       placeholder="0.00">
+                                <label for="sortByAmount">Max</label>
+                                <input type="text"
+                                       name="sortByDate"
+                                       class="form-control"
+                                       placeholder="0.00">
+                        </div>
+
+                    </div>
+                    <div class="table-responsive"
+                         style="max-height: 17em">
+                        <table class="table table-hover">
+
+
+                            <thead class="thead-dark"
+                                   style="position: sticky;">
+                                <tr>
+                                    <th scope="col">Datum</td>
+                                    <th scope="col">Empfänger/Sender</td>
+                                    <th scope="col">Betrag</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>12.12.2021</td>
+                                    <td>AT-123465436324</td>
+                                    <td>-350.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>15.12.2021</td>
+                                    <td>AT-12309638324</td>
+                                    <td>-100.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>12.12.2021</td>
+                                    <td>AT-123465436324</td>
+                                    <td>-350.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>15.12.2021</td>
+                                    <td>AT-12309638324</td>
+                                    <td>-100.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                                <tr>
+                                    <td>01.02.2022</td>
+                                    <td>AT-12348395324</td>
+                                    <td>700.00€</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+
+
+
+                <?php
+                }
+            ?>
+
+        </div>
 
 </html>
